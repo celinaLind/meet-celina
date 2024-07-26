a6d6312bf18e1f7d5f2e13c26c3936eea9171f6c
celinaLind <clind323@gmail.com>
2024-07-26 12:56:49 -0700

    remove about us from header and update text for contacting and supporting friends

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,10 +10,9 @@ export default function Header() {
       <h1>My Wonderland</h1>
       <nav>
         <ul className='navigation'>
-          <li><a href='#about'>About</a></li>
           <li><a href='#experience'>Experience</a></li>
-          <li><a href='#contact-me'>Contact</a></li>
-          <li><a href='#support'>Friends</a></li>
+          <li><a href='#contact-me'>Reach Out</a></li>
+          <li><a href='#support'>My Friends</a></li>
         </ul>
       </nav>
       </Container>
