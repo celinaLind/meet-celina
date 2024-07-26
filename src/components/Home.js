445c363e200cdbce891dd32cc7300be557de0fe1
celinaLind <clind323@gmail.com>
2024-07-26 12:34:29 -0700

    add timeline in experience section using mdb-react styling kit

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,10 +4,11 @@ import { Container, Row, Col, Form, Card, Carousel } from 'react-bootstrap';
 import SocialMedia from './SocialMedia.js';
 import Contact from './Contact.js';
 import SupportMyFriends from './SupportMyFriends.js';
+import { MDBContainer, MDBCard, MDBCardBody } from 'mdb-react-ui-kit';
 
 // will look into embedding social media posts later
-import { InstagramEmbed } from 'react-social-media-embed';
-import { TikTokEmbed } from 'react-social-media-embed';
+// import { InstagramEmbed } from 'react-social-media-embed';
+// import { TikTokEmbed } from 'react-social-media-embed';
 
 export default function Home() {
 
@@ -41,9 +42,74 @@ export default function Home() {
             <Row>
             <Col>
                 <h2 id='experience'>Experience</h2>
-                <p>
-                I have experience working in a team environment and collaborating with others. I have experience working on projects from start to finish. I have experience working with databases and APIs. I have experience working with front-end and back-end technologies.
-                </p>
+<MDBContainer fluid className="py-5" >
+      <div className="main-timeline">
+        <div className="timeline left">
+          <MDBCard>
+            <MDBCardBody className="p-4">
+              <h3>Headstarter Fellow</h3>
+              <p className="mb-0">
+                I am currently a Headstarter Fellow and college student.
+              </p>
+            </MDBCardBody>
+          </MDBCard>
+        </div>
+        <div className="timeline right">
+          <MDBCard>
+            <MDBCardBody className="p-4">
+              <h3>Junior Software Developer</h3>
+              <p className="mb-0">
+                As Novvia sales were ramping down, I was offered a full-time position at eComsystems as a junior software developer.
+              </p>
+            </MDBCardBody>
+          </MDBCard>
+        </div>
+        <div className="timeline left">
+          <MDBCard>
+            <MDBCardBody className="p-4">
+              <h3>Junior Software Developer</h3>
+              <p className="mb-0">
+                Gained a full-time position at Novvia as a junior software developer after my internship.
+                During that time I lead a team of developers and client communications to convert web applications from Php to JavaScript.
+              </p>
+            </MDBCardBody>
+          </MDBCard>
+        </div>
+        <div className="timeline right">
+          <MDBCard>
+            <MDBCardBody className="p-4">
+              <h3>Software Development Intern</h3>
+              <p className="mb-0">
+                Worked as a software development intern at a small startup, Novvia which sadly no longer exists. 
+                I worked on a team of developers to create mobile and web applications for clients. 
+                I worked on the front-end and back-end of the application. I learned a lot about software development and gained valuable experience.
+              </p>
+            </MDBCardBody>
+          </MDBCard>
+        </div>
+        <div className="timeline left">
+          <MDBCard>
+            <MDBCardBody className="p-4">
+              <h3>Graduate with Associates of Science in Computer Science</h3>
+              <p className="mb-0">
+                Woohooo!
+              </p>
+            </MDBCardBody>
+          </MDBCard>
+        </div>
+        <div className="timeline right">
+          <MDBCard>
+            <MDBCardBody className="p-4">
+              <h3>Engineering Internship</h3>
+              <p className="mb-0">
+                I did a year-long internship at Hirschvogel Incorporated. I worked on a team of engineers to maintain and optimize manufacturing operations. 
+                More to include here.
+              </p>
+            </MDBCardBody>
+          </MDBCard>
+        </div>
+      </div>
+    </MDBContainer>
             </Col>
             </Row>
         </Container>
