309863c7aa3ca4b38d0df212adf003cddd7b4a6c
celinaLind <clind323@gmail.com>
2024-07-26 11:38:43 -0700

    update formatting of form/add handling submit fcn/add primary-bg styling color

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,25 +1,39 @@
 import React from "react";
-import { Container, Form } from "react-bootstrap";
+import { Container, Form, Row, Col, Button } from "react-bootstrap";
 
 export default function Contact() {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const name = form.elements['name'].value;
+        const email = form.elements['email'].value;
+        const message = form.elements['message'].value;
+
+        const mailtoLink = `mailto:clind323@gmail.com?subject=Personal%20Site%20Contact%20Form%20Submission&body=Name:%20${encodeURIComponent(name)}%0AEmail:%20${encodeURIComponent(email)}%0AMessage:%20${encodeURIComponent(message)}`;
+        window.location.href = mailtoLink;
+    };
+
     return (
-    <Container className='mt-3'>
-        <h2 id='contact-me'>Contact Me</h2>
-        <Form>
-            <Form.Group>
-                <Form.Label>Name</Form.Label>
-                <Form.Control type='text' placeholder='Enter your name' />
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Email</Form.Label>
-                <Form.Control type='email' placeholder='Enter your email' />
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Message</Form.Label>
-                <Form.Control as='textarea' rows={3} placeholder='Enter your message' />
-            </Form.Group>
-            <button type='submit'>Submit</button>
-        </Form>
-    </Container>
+        <Container className='mt-3'>
+            <h2 id='contact-me'>Contact Me</h2>
+            <Form onSubmit={handleSubmit}>
+                <Row>
+                    <Col>
+                        <Form.Group>
+                            <Form.Control type='text' placeholder='Enter your name' />
+                        </Form.Group>
+                    </Col>
+                    <Col>
+                        <Form.Group>
+                            <Form.Control type='email' placeholder='Enter your email' />
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <Form.Group>
+                    <Form.Control className='mt-2 mb-2' as='textarea' rows={3} placeholder='Enter your message' />
+                </Form.Group>
+                <Button className='primary-bg' size="lg" type='submit'>Submit</Button>
+            </Form>
+        </Container>
     );
 }
